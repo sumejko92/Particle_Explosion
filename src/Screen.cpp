bc2ead6edbe260cd3d66961ce707331e5607814b
Pavel <p_sumejko@yahoo.com>
2019-07-14 17:20:29 +0300

    default members initialized

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -1,7 +1,12 @@
 #include "../include/Screen.h"
+#include <iostream>
 
-Screen::Screen() {}
+using namespace std;
 
+Screen::Screen()
+    : window_(NULL), renderer_(NULL), texture_(NULL), buffer_(NULL) {
+  cout << "Window constructor called" << endl;
+}
 bool Screen::init() {}
 bool Screen::processEvents() {}
 
