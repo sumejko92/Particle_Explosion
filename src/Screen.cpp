d281e93e1a040d1911edb13a3918d15c8f160376
Pavel <p_sumejko@yahoo.com>
2019-07-14 17:26:05 +0300

    createWindow defined

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -17,7 +17,37 @@ bool Screen::init() {
   return true;
 }
 
-bool Screen::createWindow() {}
+bool Screen::createWindow() {
+  window_ = SDL_CreateWindow("Particle Explosion", SDL_WINDOWPOS_UNDEFINED,
+                             SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                             SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+  if (window_ == NULL) {
+    cout << SDL_GetError() << endl;
+    return false;
+  }
+  cout << "Window created" << endl;
+
+  renderer_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_PRESENTVSYNC);
+
+  if (renderer_ == NULL) {
+    cout << " Could not return renderer" << endl;
+    return false;
+  }
+  cout << "Renderer created" << endl;
+
+  texture_ =
+      SDL_CreateTexture(renderer_, SDL_PIXELFORMAT_RGBA8888,
+                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  if (texture_ == NULL) {
+    cout << " Could not return texture" << endl;
+    return false;
+  }
+  cout << "Texture created" << endl;
+
+  return true;
+}
 
 bool Screen::processEvents() {}
 
